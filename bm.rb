e7d30c49637f12eba54ae2cf6608c75d7f729d75
John Bracken <johnpatrickbracken@gmail.com>
2012-05-02 02:49:04 +0100

    More minor fixes

--- bm.rb
+++ bm.rb
@@ -1,36 +1,22 @@
 ﻿class BM
-  def initialize neurons
+  def initialize(neurons,examples)
     @neurons = neurons
+    @examples = examples
+    @samples = 500
     @net = Array.new(neurons)
     @w = Array.new(@neurons) { Array.new(@neurons) {0} }
     @dw = Array.new(@neurons) { Array.new(@neurons) {0} }
-    @examples = Array.new(neurons) { Array.new(neurons) {0} }
-    @max_flips = 10
+    @example = Array.new(@examples) { Array.new(@neurons) {0} }
+    @max_flips = 100
     @previous_temp = 0
     @current_temp = 0
     @eta = 0.01  # learning rate
-    @samples = 3
   end
   
   def init_random  # initBM with small random values
     for i in 0..@neurons-1
       for j in 0..@neurons-1 do
         @w[i][j] = 0.2*(rand()-0.5)
-      end
-    end
-  end
-  
-  def flip
-    for q in 0..@max_flips-1
-      for j in 0..@neurons-1
-        z = activate(j)
-        temp = 0.1*(@max_flips-q)/@max_flips
-        if (1/(1+exp(-2*z/temp)) > rand())
-          @net[j] = 1
-        else
-          @net[j] = -1
-        end
-        puts @net[j]
       end
     end
   end
@@ -45,21 +31,16 @@
     z
   end
   
-  def converged?
-    @previous_temp == @current_temp
-  end
-  
   def dw
-    for i in 0..@samples-1
+    for i in 0..@examples-1
       for j in 0..@neurons-1
         for k in 0..@neurons-1
-          @dw[j][k] += @eta*@examples[i][j]*@examples[i][k]
+          @dw[j][k] += @eta*@example[i][j]*@example[i][k]
         end
       end
     end
     
-    # init @net with random start
-    for i in 0..@samples - 1
+    for i in 0..@samples-1
       for j in 0..@neurons-1
         if rand() < 0.5
           @net[j] = -1
@@ -67,14 +48,46 @@
           @net[j] = 1
         end
       end
+      
+      for q in 0..@max_flips-1
+        for j in 0..@neurons-1
+          z = activate(j)
+          @current_temp = 0.1*(@max_flips-q)/@max_flips
+          if (1/(1+Math.exp(-2*z/@current_temp)) > rand())
+            @net[j] = 1
+          else
+            @net[j] = -1
+          end
+        end
+      end
+      
+      for j in 0..@neurons-1
+        for k in 0..@neurons-1
+          @dw[j][k] -= @eta*@neurons*@net[j]*@net[k] / @samples
+        end
+      end
     end
+  end
   
-    until self.converged?
-      flip
-    end
-  end # end dw
+  def test1
+    initialize(6,3)
+    init_random
+    @example[0] = [-1,-1,-1,-1,-1,1]
+    @example[1] = [-1,-1,1,-1,-1,-1]
+    @example[2] = [1,-1,-1,-1,-1,-1]
+    self.dw
+  end
+  
+  def test2
+    initialize(10,2)
+    init_random
+    @example[0] = [-1, -1, -1, -1, -1, -1, -1, -1, -1, 1]
+    @example[1] = [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    self.dw
+  end
 end
 
-test = BM.new 6
+test = BM.new 6,3
 test.init_random
-test.dw
+test.test1
+test.test2
